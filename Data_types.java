5d3efc79043aa690de6103bd3844d286f1a50849
Anu Kumari <87594517+anu1501@users.noreply.github.com>
2022-08-22 21:28:58 +0530

    Add files via upload

---
+++ Data_types.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Data_types {
+   
+    public static void main(String[] args) throws IOException {
+        int i = 4;
+        int t = 8;
+        double d = 4.0;
+        double f = 8.0;
+        String s = "HackerRank ";
+        String str="is very helpful.";
+        
+       
+        System.out.println(i+t);
+        System.out.println(d+f);
+        System.out.println(s+str);
+    }
+}
+
+
